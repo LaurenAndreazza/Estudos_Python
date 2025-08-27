c366c3abd5472afd1e080c32b2b81518d5776079
Lauren Andreazza <129696871+LaurenAndreazza@users.noreply.github.com>
2025-08-27 12:27:31 -0300

    Atualizar o variaveis-conceitos.py

--- variaveis-conceitos.py
+++ variaveis-conceitos.py
@@ -1,12 +1,1 @@
-meuamor = "madu, me diga"
-
-amordeMadu = float(input("Diga o quanto me ama:"))
-
-def calcular_amor(amordeMadu):
-
-    if amordeMadu< 100:
-        return "não me ama o suficiente"
-    else:
-        return "Acho que ta bom"
-
-print(calcular_amor(amordeMadu))
+teste = 1
